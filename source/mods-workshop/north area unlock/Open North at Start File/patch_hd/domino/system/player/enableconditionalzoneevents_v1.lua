a4e52480446fed2b8a7ce5fac1537208f8062ae2
LinkHero95 <linkhero95@gmail.com>
2021-05-13 09:08:54 +0530

    Removed ACE unlock mod from workshop.

---
+++ source/mods-workshop/north area unlock/Open North at Start File/patch_hd/domino/system/player/enableconditionalzoneevents_v1.lua
@@ -0,0 +1,20 @@
+EnableConditionalZoneEvents = {}
+function EnableConditionalZoneEvents:Create(cbox)
+  cbox:RegisterLibrary("Domino/System/LuaLibraries/LuaLibCore.lua")
+  cbox:RegisterLibrary("Domino/System/LuaLibraries/LuaLibStateless.lua")
+end
+function EnableConditionalZoneEvents:SetState(zoneId, state)
+  SetConditionalZoneIdState(zoneId, state)
+end
+function EnableConditionalZoneEvents:Lock()
+  local out = self.Locked
+  self:SetState(self.zoneID, false)
+  out(self)
+end
+function EnableConditionalZoneEvents:Unlock()
+  local out = self.Unlocked
+  self:SetState(self.zoneID, false)
+  out(self)
+end
+export = EnableConditionalZoneEvents
+EnableConditionalZoneEvents = nil
